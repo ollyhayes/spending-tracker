5a79cbb98ed8e6f1d252e4ee2e0d318767a7d4b9
Olly Hayes <olly.hayes@gmail.com>
2017-09-04 13:18:17 +0900

    Remove expenditure list for now

--- expenditure-list.jsx
+++ expenditure-list.jsx
@@ -23,26 +23,28 @@ export default class ExpenditureList extends React.Component
 
 	render()
 	{
-		return <table>
-			<thead>
-				<tr>
-					<th>Date</th>
-					<th>Category</th>
-					<th>Description</th>
-					<th>Amount</th>
-				</tr>
-			</thead>
-			<tbody>
-				{
-					this.state.expenditures.map(expenditure =>
-						<tr key={expenditure.date.getTime()}>
-							<td>{expenditure.date.toString()}</td>
-							<td>{expenditure.category}</td>
-							<td>{expenditure.description}</td>
-							<td>{expenditure.amount}</td>
-						</tr>)
-				}
-			</tbody>
-		</table>;
+		return <div className="expenditure-list">
+			<table>
+				<thead>
+					<tr>
+						<th>Date</th>
+						<th>Category</th>
+						<th>Description</th>
+						<th>Amount</th>
+					</tr>
+				</thead>
+				<tbody>
+					{
+						this.state.expenditures.map(expenditure =>
+							<tr key={expenditure.date.getTime()}>
+								<td>{expenditure.date.toString()}</td>
+								<td>{expenditure.category}</td>
+								<td>{expenditure.description}</td>
+								<td>{expenditure.amount}</td>
+							</tr>)
+					}
+				</tbody>
+			</table>
+		</div>;
 	}
 }
--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import InputForm from "./input-form.jsx";
-import ExpenditureList from "./expenditure-list.jsx";
+//import ExpenditureList from "./expenditure-list.jsx";
 import SpendingManager from "./spending-manager.js";
 
 class Content extends React.Component
@@ -17,8 +17,10 @@ class Content extends React.Component
 		return <div>
 			<header>Travel Expenses</header>
 			<InputForm spendingManager={this.spendingManager}/>
-			<ExpenditureList spendingManager={this.spendingManager}/>
 		</div>;
+
+		// leaving this out for now
+		// <ExpenditureList spendingManager={this.spendingManager}/>
 	}
 }
 
