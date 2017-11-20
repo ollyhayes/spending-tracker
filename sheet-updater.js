2f6c5d936e587888c7d3bd089c27e6adb72e16dd
Olly Hayes <olly.hayes@gmail.com>
2017-11-20 15:37:19 +0700

    Autofill some columns

--- sheet-updater.js
+++ sheet-updater.js
@@ -10,7 +10,22 @@ export const status = {
 
 //const spreadsheetId = "1_WgnEfEjsM0EvyDkOq9U1iGbmcno4tGWZZUWyp8975w"; // test sheet
 const spreadsheetId = "19wRTaZ6ESmu4l2M7uV46OwDn5N3e-fzpgHSXdWChrOU";
+const sheetId = 1137143099;
 const apiKey = "AIzaSyBlVXClFKOd0SxDFMBmgZYcOZJdM0LUW0I";
+
+const autoFillColumns = [
+	{ startColumnIndex: 2, endColumnIndex: 3 }, // Location column (C)
+	{ startColumnIndex: 6, endColumnIndex: 9 }, // Currency columns (G, H, I)
+];
+
+function getFinalRowIndexFrom(range)
+{
+	const finalRowIndexRegex = /.*![A-Z]+\d+:[A-Z]+(\d+)/; // e.g. "Spending!A587:F588" matching 588 in group 1
+
+	const result = finalRowIndexRegex.exec(range);
+
+	return Number(result[1]);
+}
 
 export default class SheetUpdater
 {
@@ -20,16 +35,28 @@ export default class SheetUpdater
 	{
 		this.status = status.attemptingSync;
 
-		const itemsAppended = await this._tryAppendItems(expenditures, accessToken);
+		const {success, response} = await this._tryAppendItems(expenditures, accessToken);
 
-		if (itemsAppended)
-			await this._trySortItems(accessToken);
+		if (success)
+		{
+			const finalRowIndex = getFinalRowIndexFrom(response.updates.updatedRange);
 
-		this.status = itemsAppended
+			const autofillRequests = autoFillColumns.map(indices =>
+				this._getAutofillRequest(expenditures.length, finalRowIndex, indices));
+
+			await this._tryBatchRequest(
+				[
+					...autofillRequests,
+					this._getSortItemsRequest()
+				],
+				accessToken);
+		}
+
+		this.status = success
 			? status.synced
 			: status.noConnection;
 
-		return itemsAppended;
+		return success;
 	}
 
 	_tryAppendItems(expenditures, accessToken)
@@ -65,43 +92,62 @@ export default class SheetUpdater
 		return this._tryMakeRequest(url, JSON.stringify(body), accessToken);
 	}
 
-	_trySortItems(accessToken)
+	_getSortItemsRequest()
+	{
+		return {
+			sortRange: {
+				range: {
+					sheetId,
+					startRowIndex: 1,
+					endRowIndex: null,
+					startColumnIndex: 0,
+					endColumnIndex: null
+				},
+				sortSpecs: [
+					{
+						dimensionIndex: 0, // Date column
+						sortOrder: "ASCENDING"
+					},
+					{
+						dimensionIndex: 1, // Time column
+						sortOrder: "ASCENDING"
+					}
+				]
+			}
+		};
+	}
+
+	_getAutofillRequest(numberOfExpendituresAdded, finalRowIndex, { startColumnIndex, endColumnIndex })
+	{
+		return {
+			autoFill: {
+				range: {
+					sheetId,
+					startRowIndex: finalRowIndex - numberOfExpendituresAdded,
+					endRowIndex: finalRowIndex + 1,
+					startColumnIndex,
+					endColumnIndex
+				}
+			}
+		};
+	}
+
+	_tryBatchRequest(requests, accessToken)
 	{
 		const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}:batchUpdate`;
 
 		const body = {
-			requests: [
-				{
-					sortRange: {
-						range: {
-							sheetId: 1137143099, // Spending sheet ID
-							startRowIndex: 1,
-							endRowIndex: null,
-							startColumnIndex: 0,
-							endColumnIndex: null
-						},
-						sortSpecs: [
-							{
-								dimensionIndex: 0, // Date column
-								sortOrder: "ASCENDING"
-							},
-							{
-								dimensionIndex: 1, // Time column
-								sortOrder: "ASCENDING"
-							}
-						]
-					}
-				}
-			],
+			requests: requests,
 			includeSpreadsheetInResponse: false
 		};
 
 		return this._tryMakeRequest(url, JSON.stringify(body), accessToken);
 	}
 
+
 	_tryMakeRequest(url, body, accessToken)
 	{
-		return new Promise(resolve =>
+		return new Promise((resolve, reject) =>
 		{
 			const request = new XMLHttpRequest();
 			request.open("POST", url, true);
@@ -113,10 +159,19 @@ export default class SheetUpdater
 
 			request.onreadystatechange = () =>
 			{
-				if (request.readyState !== XMLHttpRequest.DONE)
+				if (request.readyState !== XMLHttpRequest.DONE)	// not finished yet
 					return;
-
-				resolve(request.status === 200);
+				else if (request.status === 200)				// success
+					resolve({
+						success: true,
+						response: JSON.parse(request.response)
+					});
+				else if (request.status < 100)					// no internet connection
+					resolve({
+						success: false
+					});
+				else
+					reject(request.response);					// other unknown error e.g. 400
 			};
 		});
 	}
