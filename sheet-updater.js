7607b35643636476bb942cb5541c174c80c6b524
Olly Hayes <olly.hayes@gmail.com>
2017-11-20 08:39:34 +0700

    Refactor sheet updater for multiple operations

--- sheet-updater.js
+++ sheet-updater.js
@@ -10,18 +10,7 @@ export const status = {
 
 //const spreadsheetId = "1_WgnEfEjsM0EvyDkOq9U1iGbmcno4tGWZZUWyp8975w"; // test sheet
 const spreadsheetId = "19wRTaZ6ESmu4l2M7uV46OwDn5N3e-fzpgHSXdWChrOU";
-const range = "A1";
 const apiKey = "AIzaSyBlVXClFKOd0SxDFMBmgZYcOZJdM0LUW0I";
-
-const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}:append`;
-
-const queryString = "responseValueRenderOption=FORMATTED_VALUE" +
-	"&insertDataOption=INSERT" + 
-	"&valueInputOption=USER_ENTERED" + 
-	"&responseDateTimeRenderOption=FORMATTED_STRING" + 
-	"&includeValuesInResponse=false" + 
-	"&alt=json" + 
-	`&key=${apiKey}`;
 
 export default class SheetUpdater
 {
@@ -31,15 +20,59 @@ export default class SheetUpdater
 	{
 		this.status = status.attemptingSync;
 
+		const itemsAppended = this._appendItems(expenditures, accessToken);
+
+		this.status = itemsAppended
+			? status.synced
+			: status.noConnection;
+
+		return itemsAppended;
+	}
+
+	async _appendItems(expenditures, accessToken)
+	{
+		const range = "A1";
+		const baseUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}:append`;
+
+		const queryString = "responseValueRenderOption=FORMATTED_VALUE" +
+			"&insertDataOption=INSERT" + 
+			"&valueInputOption=USER_ENTERED" + 
+			"&responseDateTimeRenderOption=FORMATTED_STRING" + 
+			"&includeValuesInResponse=false" + 
+			"&alt=json" + 
+			`&key=${apiKey}`;
+
+		const url = `${baseUrl}?${queryString}`;
+		const body = {
+			values: expenditures.map(expenditure =>
+			{
+				const date = moment(expenditure.date);
+
+				return [
+					date.format("DD/MM/YYYY"),
+					date.format("HH:mm:ss"),
+					"",
+					expenditure.category,
+					expenditure.description,
+					expenditure.amount
+				];
+			})
+		};
+
+		return await this._makeRequest(url, JSON.stringify(body), accessToken);
+	}
+
+	_makeRequest(url, body, accessToken)
+	{
 		return new Promise(resolve =>
 		{
 			const request = new XMLHttpRequest();
-			request.open("POST", `${url}?${queryString}`, true);
+			request.open("POST", url, true);
 
 			request.setRequestHeader("Content-type", "application/json");
 			request.setRequestHeader("Authorization", `Bearer ${accessToken}`);
 
-			request.send(this._getJsonBody(expenditures));
+			request.send(body);
 
 			request.onreadystatechange = () =>
 			{
@@ -58,26 +91,5 @@ export default class SheetUpdater
 				}
 			};
 		});
-	}
-
-	_getJsonBody(expenditures)
-	{
-		const body = {
-			values: expenditures.map(expenditure =>
-			{
-				const date = moment(expenditure.date);
-
-				return [
-					date.format("DD/MM/YYYY"),
-					date.format("HH:mm:ss"),
-					"",
-					expenditure.category,
-					expenditure.description,
-					expenditure.amount
-				];
-			})
-		};
-
-		return JSON.stringify(body);
 	}
 }
