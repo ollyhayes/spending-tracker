3cc39581617069ee24bd8c19fd98ef0c91f28473
Olly Hayes <olly.hayes@gmail.com>
2017-12-03 09:11:48 +0700

    Minify javascript

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
+const Uglify = require("uglifyjs-webpack-plugin");
 const path = require("path");
 
-module.exports = {
-	entry: path.resolve(__dirname, "scripts/entry.js"),
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "public")
-	},
-	devtool: "#source-map",
-	module: {
-		loaders: [
-			{ test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
-			{ test: /\.less$/, exclude: /node_modules/, loader: "style-loader!css-loader!less-loader" }
-		]
-	}
+module.exports = env =>
+{
+	const config = {
+		entry: path.resolve(__dirname, "scripts/entry.js"),
+		output: {
+			filename: "bundle.js",
+			path: path.resolve(__dirname, "public")
+		},
+		devtool: "#source-map",
+		module: {
+			loaders: [
+				{ test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
+				{ test: /\.less$/, exclude: /node_modules/, loader: "style-loader!css-loader!less-loader" }
+			]
+		},
+		plugins: []
+	};
+
+	if (env && env.production)
+		config.plugins.push(new Uglify());
+
+	return config;
 };
