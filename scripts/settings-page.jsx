66a5d43b962f83556727eb8f27aaf9c228f67017
Olly Hayes <olly.hayes@gmail.com>
2018-01-30 12:09:37 +0800

    Adding settings page

---
+++ scripts/settings-page.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export default class SettingsPage extends React.Component
+{
+	constructor(props, logger)
+	{
+		super();
+	}
+
+	render()
+	{
+		return <div>
+			<button>Debug info</button>
+		</div>;
+	}
+}
