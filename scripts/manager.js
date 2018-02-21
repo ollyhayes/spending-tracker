6d54f074447ce77f44d64682de8277aef9abfee6
Olly Hayes <olly.hayes@gmail.com>
2018-02-21 14:47:52 +1100

    Make manager members private

--- scripts/manager.js
+++ scripts/manager.js
@@ -9,62 +9,62 @@ export default class Manager
 {
 	constructor(logger)
 	{
-		this.spendingManager = new SpendingManager();
-		this.accountManager = new AccountManager(logger);
-		this.sheetUpdater = new SheetUpdater();
-		this.logger = logger;
+		this._spendingManager = new SpendingManager();
+		this._accountManager = new AccountManager(logger);
+		this._sheetUpdater = new SheetUpdater();
+		this._logger = logger;
 
-		this.syncQueud = false;
-		this.syncInProgress = false;
-		this.syncNumber = 0;
+		this._syncQueud = false;
+		this._syncInProgress = false;
+		this._syncNumber = 0;
 
 		this.sync();
 	}
 
-	@computed get accountStatus() { return this.accountManager.status; }
-	@computed get accountUsername() { return this.accountManager.username; }
+	@computed get accountStatus() { return this._accountManager.status; }
+	@computed get accountUsername() { return this._accountManager.username; }
 
-	@computed get syncStatus() { return this.sheetUpdater.status; }
+	@computed get syncStatus() { return this._sheetUpdater.status; }
 
-	@computed get numberOfItemsAwaitingSync() { return this.spendingManager.expenditures.length; }
+	@computed get numberOfItemsAwaitingSync() { return this._spendingManager.expenditures.length; }
 
 	signIn()
 	{
-		this.accountManager.signIn();
+		this._accountManager.signIn();
 	}
 
 	signOut()
 	{
-		this.accountManager.signOut();
+		this._accountManager.signOut();
 	}
 
 	addExpenditure(date, category, amount, description)
 	{
-		this.spendingManager.addExpenditure(date, category, amount, description);
+		this._spendingManager.addExpenditure(date, category, amount, description);
 
 		this.sync();
 	}
 
 	async sync()
 	{
-		const syncNumber = this.syncNumber++;
-		const log = message => this.logger.log(`S${syncNumber} - ${message}`);
+		const syncNumber = this._syncNumber++;
+		const log = message => this._logger.log(`S${syncNumber} - ${message}`);
 
 		log("Starting...");
 
-		if (this.syncInProgress)
+		if (this._syncInProgress)
 		{
 			log("Sync in progress - queueing");
-			this.syncQueued = true;
+			this._syncQueued = true;
 			return;
 		}
 
-		this.syncInProgress = true;
+		this._syncInProgress = true;
 
 		try
 		{
 			if (this.accountStatus === accountStatus.notConnected)
-				await this.accountManager.initialise();
+				await this._accountManager.initialise();
 
 			if (this.accountStatus !== accountStatus.signedIn)
 			{
@@ -72,7 +72,7 @@ export default class Manager
 				return;
 			}
 
-			const currentExpenditures = this.spendingManager.expenditures.slice();
+			const currentExpenditures = this._spendingManager.expenditures.slice();
 
 			if (currentExpenditures.length === 0)
 			{
@@ -82,12 +82,12 @@ export default class Manager
 
 			log(`Attempting sync - ${currentExpenditures.length} expenditures`);
 
-			const success = await this.sheetUpdater.trySync(currentExpenditures, this.accountManager.getAccessToken());
+			const success = await this._sheetUpdater.trySync(currentExpenditures, this._accountManager.getAccessToken());
 
 			if (success)
 			{
 				log("Sync succeded");
-				this.spendingManager.clearExpenditures(currentExpenditures);
+				this._spendingManager.clearExpenditures(currentExpenditures);
 			}
 			else
 			{
@@ -96,12 +96,12 @@ export default class Manager
 		}
 		finally
 		{
-			this.syncInProgress = false;
+			this._syncInProgress = false;
 		}
 
-		if (this.syncQueued)
+		if (this._syncQueued)
 		{
-			this.syncQueued = false;
+			this._syncQueued = false;
 			await this.sync();
 		}
 	}
