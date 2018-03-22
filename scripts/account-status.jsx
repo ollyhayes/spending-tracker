ce8ffda3d8832bf926d563502d0c2ed2c485c0a5
Olly Hayes <olly.hayes@gmail.com>
2018-03-22 11:33:44 -0300

    Update account status to handle loading/not connected

--- scripts/account-status.jsx
+++ scripts/account-status.jsx
@@ -28,11 +28,30 @@ export default class AccountStatus extends React.Component
 	render()
 	{
 		return <div className="account-status">
-			{
-				this.manager.accountStatus === accountStatus.signedIn
-					? <span className="good-message">{this.manager.accountUsername}<a href="javascript:void(0)" onClick={this.handleSignOut}>Sign out</a></span>
-					: <span className="neutral-message">Signed out<a href="javascript:void(0)" onClick={this.handleSignIn}>Sign in</a></span>
-			}
+			{ this._getStatus() }
 		</div>;
+	}
+
+	_getStatus()
+	{
+		switch (this.manager.accountStatus)
+		{
+		case accountStatus.signedIn:
+			return <span className="good-message">
+				{this.manager.accountUsername}<a href="javascript:void(0)" onClick={this.handleSignOut}>Sign out</a>
+			</span>;
+		case accountStatus.signedOut:
+			return <span className="neutral-message">
+				Signed out<a href="javascript:void(0)" onClick={this.handleSignIn}>Sign in</a>
+			</span>;
+		case accountStatus.loading:
+			return <span className="neutral-message">
+				Loading...
+			</span>;
+		case accountStatus.notConnected:
+			return <span className="bad-message">
+				Not connected
+			</span>;
+		}
 	}
 }
