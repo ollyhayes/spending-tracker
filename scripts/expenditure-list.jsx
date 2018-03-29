d43b22f6eba644860b13a0f4117429961f843249
Olly Hayes <olly.hayes@gmail.com>
2018-03-29 11:42:26 -0300

    Show category if no description given

--- scripts/expenditure-list.jsx
+++ scripts/expenditure-list.jsx
@@ -45,7 +45,7 @@ export default class ExpenditureList extends React.Component
 												: timestamp("DD/MM/YY", expenditure.date)
 										}
 									</td>
-									<td>{expenditure.description}</td>
+									<td>{expenditure.description || expenditure.category}</td>
 									<td>{expenditure.amount}</td>
 									<td>
 										{
