f231801ff2d18796d2edbe230f0a4208f7ef72df
Olly Hayes <olly.hayes@gmail.com>
2018-06-18 10:13:26 -0500

    Make recent items scrollable

--- scripts/expenditure-list.jsx
+++ scripts/expenditure-list.jsx
@@ -24,41 +24,43 @@ export default class ExpenditureList extends React.Component
 			<header>
 				Recent expenditures:
 			</header>
-			<table>
-				<thead>
-					<tr>
-						<th>Date</th>
-						<th>Description</th>
-						<th>Price</th>
-						<th>Sync</th>
-					</tr>
-				</thead>
-				<tbody>
-					{
-						this.manager.allExpenditures.length > 0
-							? this.manager.allExpenditures.map(expenditure =>
-								<tr key={expenditure.date.getTime()}>
-									<td>
-										{
-											isSameDay(new Date(), expenditure.date)
-												? timestamp("HH:mm", expenditure.date)
-												: timestamp("DD/MM/YY", expenditure.date)
-										}
-									</td>
-									<td>{expenditure.description || expenditure.category}</td>
-									<td>{expenditure.amount}</td>
-									<td>
-										{
-											expenditure.synced
-												? <span className="good-message"><Icon iconName="check"/></span>
-												: <span className="bad-message"><Icon iconName="times"/></span>
-										}
-									</td>
-								</tr>).reverse()
-							: <tr><td colSpan="5">No expenditures logged</td></tr>
-					}
-				</tbody>
-			</table>
+			<div>
+				<table>
+					<thead>
+						<tr>
+							<th>Date</th>
+							<th>Description</th>
+							<th>Price</th>
+							<th>Sync</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							this.manager.allExpenditures.length > 0
+								? this.manager.allExpenditures.map(expenditure =>
+									<tr key={expenditure.date.getTime()}>
+										<td>
+											{
+												isSameDay(new Date(), expenditure.date)
+													? timestamp("HH:mm", expenditure.date)
+													: timestamp("DD/MM/YY", expenditure.date)
+											}
+										</td>
+										<td>{expenditure.description || expenditure.category}</td>
+										<td>{expenditure.amount}</td>
+										<td>
+											{
+												expenditure.synced
+													? <span className="good-message"><Icon iconName="check"/></span>
+													: <span className="bad-message"><Icon iconName="times"/></span>
+											}
+										</td>
+									</tr>).reverse()
+								: <tr><td colSpan="5">No expenditures logged</td></tr>
+						}
+					</tbody>
+				</table>
+			</div>
 		</div>;
 	}
 }
